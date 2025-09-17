9fc44f7518c633d8dc557b6aff5c4ebdbc3aa5f7
Hoppy-days <fifthstreetjammer@gmail.com>
2025-09-17 16:53:39 -0500

    Register market block entity renderer

--- src/main/java/net/jeremy/gardenkingmod/GardenKingModClient.java
+++ src/main/java/net/jeremy/gardenkingmod/GardenKingModClient.java
@@ -1,13 +1,15 @@
 package net.jeremy.gardenkingmod;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.gui.screen.ingame.HandledScreens;
-
+import net.jeremy.gardenkingmod.client.render.MarketBlockEntityRenderer;
 import net.jeremy.gardenkingmod.screen.MarketScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class GardenKingModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HandledScreens.register(ModScreenHandlers.MARKET_SCREEN_HANDLER, MarketScreen::new);
+        BlockEntityRendererFactories.register(ModBlockEntities.MARKET_BLOCK_ENTITY, MarketBlockEntityRenderer::new);
     }
 }
