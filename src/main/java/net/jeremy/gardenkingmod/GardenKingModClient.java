efe6169bc7145e2af28f9f4689279e0ed4a3f896
Hoppy-days <fifthstreetjammer@gmail.com>
2025-09-17 17:59:34 -0500

    Register market block model layer

--- src/main/java/net/jeremy/gardenkingmod/GardenKingModClient.java
+++ src/main/java/net/jeremy/gardenkingmod/GardenKingModClient.java
@@ -1,6 +1,8 @@
 package net.jeremy.gardenkingmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.jeremy.gardenkingmod.client.model.MarketBlockModel;
 import net.jeremy.gardenkingmod.client.render.MarketBlockEntityRenderer;
 import net.jeremy.gardenkingmod.screen.MarketScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
@@ -10,6 +12,7 @@ public class GardenKingModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HandledScreens.register(ModScreenHandlers.MARKET_SCREEN_HANDLER, MarketScreen::new);
+        EntityModelLayerRegistry.registerModelLayer(MarketBlockModel.LAYER_LOCATION, MarketBlockModel::getTexturedModelData);
         BlockEntityRendererFactories.register(ModBlockEntities.MARKET_BLOCK_ENTITY, MarketBlockEntityRenderer::new);
     }
 }
