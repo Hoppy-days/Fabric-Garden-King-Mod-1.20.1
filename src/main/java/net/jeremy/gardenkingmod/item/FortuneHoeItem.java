8ff3e7befa4f12541d1df6f0d497eca89d4ef548
Hoppy-days <fifthstreetjammer@gmail.com>
2025-09-24 15:41:36 -0500

    Format FortuneHoeItem with spaces

--- src/main/java/net/jeremy/gardenkingmod/item/FortuneHoeItem.java
+++ src/main/java/net/jeremy/gardenkingmod/item/FortuneHoeItem.java
@@ -8,16 +8,16 @@ import net.minecraft.item.ToolMaterial;
  * A hoe item that provides a built-in fortune level without requiring an enchantment.
  */
 public class FortuneHoeItem extends HoeItem implements FortuneProvidingItem {
-        private final int fortuneLevel;
+    private final int fortuneLevel;
 
-        public FortuneHoeItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings,
-                        int fortuneLevel) {
-                super(toolMaterial, attackDamage, attackSpeed, settings);
-                this.fortuneLevel = fortuneLevel;
-        }
+    public FortuneHoeItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings,
+            int fortuneLevel) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+        this.fortuneLevel = fortuneLevel;
+    }
 
-        @Override
-        public int gardenkingmod$getFortuneLevel(ItemStack stack) {
-                return fortuneLevel;
-        }
+    @Override
+    public int gardenkingmod$getFortuneLevel(ItemStack stack) {
+        return fortuneLevel;
+    }
 }
