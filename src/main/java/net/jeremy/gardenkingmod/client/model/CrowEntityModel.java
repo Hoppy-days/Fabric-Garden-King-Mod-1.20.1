2839a4d8323c7d283dead149f2b71fa4424c96e6
Hoppy-days <fifthstreetjammer@gmail.com>
2025-09-25 22:55:59 -0500

    Fix crow model import package

--- src/main/java/net/jeremy/gardenkingmod/client/model/CrowEntityModel.java
+++ src/main/java/net/jeremy/gardenkingmod/client/model/CrowEntityModel.java
@@ -8,10 +8,10 @@ import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.model.ModelPartBuilder;
 import net.minecraft.client.model.ModelPartData;
 import net.minecraft.client.model.ModelTransform;
-import net.minecraft.client.model.SinglePartEntityModel;
 import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
+import net.minecraft.client.render.entity.model.SinglePartEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
