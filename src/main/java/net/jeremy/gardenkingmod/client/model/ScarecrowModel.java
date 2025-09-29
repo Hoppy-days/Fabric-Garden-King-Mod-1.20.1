5bab0c9447c8ff02111f8e7e68124d93bb2b7532
Jeremy <fifthstreetjammer@gmail.com>
2025-09-29 02:53:25 -0500

    fixing scarecrow

--- src/main/java/net/jeremy/gardenkingmod/client/model/ScarecrowModel.java
+++ src/main/java/net/jeremy/gardenkingmod/client/model/ScarecrowModel.java
@@ -18,38 +18,25 @@ import net.minecraft.util.Identifier;
 // Made with Blockbench 4.12.6
 // Exported for Minecraft version 1.17+ for Yarn
 // Paste this class into your mod and generate all required imports
-public class ScarecrowModel extends EntityModel<Entity> {
-    public static final EntityModelLayer LAYER_LOCATION = new EntityModelLayer(
-            new Identifier(GardenKingMod.MOD_ID, "scarecrow"),
-            "main"
-    );
-
-    private final ModelPart scarecrowBase;
-
-    public ScarecrowModel(ModelPart root) {
-        this.scarecrowBase = root.getChild("scarecrow_base");
+public class scarecrow_base extends EntityModel<Entity> {
+    private final ModelPart bone;
+    public scarecrow_base(ModelPart root) {
+        this.bone = root.getChild("bone");
     }
-
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-        modelPartData.addChild("scarecrow_base", ModelPartBuilder.create()
-                        .uv(0, 12)
-                        .cuboid(-8.0F, -4.0F, -8.0F, 16.0F, 4.0F, 16.0F, new Dilation(0.0F))
-                        .uv(0, 28)
-                        .cuboid(-1.0F, -16.0F, -1.0F, 2.0F, 12.0F, 2.0F, new Dilation(0.0F))
-                        .uv(0, 24)
-                        .cuboid(-8.0F, -16.0F, -1.0F, 16.0F, 2.0F, 2.0F, new Dilation(0.0F)),
-                ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+        ModelPartData bone = modelPartData.addChild("bone", ModelPartBuilder.create().uv(0, 0).cuboid(-6.0F, -2.0F, -6.0F, 12.0F, 2.0F, 12.0F, new Dilation(0.0F))
+                .uv(0, 14).cuboid(-4.0F, -4.0F, -4.0F, 8.0F, 2.0F, 8.0F, new Dilation(0.0F))
+                .uv(0, 28).cuboid(-1.0F, -14.0F, -1.0F, 2.0F, 10.0F, 2.0F, new Dilation(0.0F))
+                .uv(0, 24).cuboid(-8.0F, -16.0F, -1.0F, 16.0F, 2.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
         return TexturedModelData.of(modelData, 64, 64);
     }
-
     @Override
     public void setAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
     }
-
     @Override
     public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
-        scarecrowBase.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+        bone.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
     }
 }
