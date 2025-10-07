33560e2dbab437cb23ec7107cf7c6c5d03cfab44
Hoppy-days <fifthstreetjammer@gmail.com>
2025-10-07 00:39:11 -0500

    Add blue sapphire equipment set

---
+++ src/main/java/net/jeremy/gardenkingmod/item/BlueSapphireToolMaterial.java
@@ -0,0 +1,45 @@
+package net.jeremy.gardenkingmod.item;
+
+import net.jeremy.gardenkingmod.ModItems;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public enum BlueSapphireToolMaterial implements ToolMaterial {
+        INSTANCE;
+
+        private static final int DURABILITY = 1879;
+        private static final float MINING_SPEED = 9.0F;
+        private static final float ATTACK_DAMAGE = 4.5F;
+        private static final int MINING_LEVEL = 4;
+        private static final int ENCHANTABILITY = 22;
+
+        @Override
+        public int getDurability() {
+                return DURABILITY;
+        }
+
+        @Override
+        public float getMiningSpeedMultiplier() {
+                return MINING_SPEED;
+        }
+
+        @Override
+        public float getAttackDamage() {
+                return ATTACK_DAMAGE;
+        }
+
+        @Override
+        public int getMiningLevel() {
+                return MINING_LEVEL;
+        }
+
+        @Override
+        public int getEnchantability() {
+                return ENCHANTABILITY;
+        }
+
+        @Override
+        public Ingredient getRepairIngredient() {
+                return Ingredient.ofItems(ModItems.BLUE_SAPPHIRE);
+        }
+}
