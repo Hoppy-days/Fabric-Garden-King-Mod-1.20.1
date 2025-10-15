253245d8d8211828ccbb02cc71701250e7c33829
Hoppy-days <fifthstreetjammer@gmail.com>
2025-10-15 11:31:54 -0500

    Keep sprinkler rotation pivot centered

--- src/main/java/net/jeremy/gardenkingmod/client/model/SprinklerModel.java
+++ src/main/java/net/jeremy/gardenkingmod/client/model/SprinklerModel.java
@@ -9,8 +9,8 @@ import net.minecraft.client.model.ModelPartData;
 import net.minecraft.client.model.ModelTransform;
 import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
@@ -23,6 +23,8 @@ public class SprinklerModel extends EntityModel<Entity> {
          * {@code assets/gardenkingmod/animations/sprinkler.animation.json}.
          */
 
+        private final SprinklerRotationAnimation rotationAnimation;
+        private final ModelPart rotationRoot;
         private final ModelPart rotation;
         private final ModelPart cap4;
         private final ModelPart bbMain;
@@ -30,7 +32,9 @@ public class SprinklerModel extends EntityModel<Entity> {
         private float rotationAngle;
 
         public SprinklerModel(ModelPart root) {
-                this.rotation = root.getChild("rotation");
+                this.rotationAnimation = SprinklerRotationAnimation.load();
+                this.rotationRoot = root.getChild("rotation_root");
+                this.rotation = this.rotationRoot.getChild("rotation");
                 this.cap4 = root.getChild("cap4");
                 this.bbMain = root.getChild("bb_main");
         }
@@ -38,22 +42,22 @@ public class SprinklerModel extends EntityModel<Entity> {
         public static TexturedModelData getTexturedModelData() {
                 ModelData modelData = new ModelData();
                 ModelPartData modelPartData = modelData.getRoot();
-                ModelPartData rotation = modelPartData.addChild("rotation", ModelPartBuilder.create(),
+                ModelPartData rotationRoot = modelPartData.addChild("rotation_root", ModelPartBuilder.create(),
                                 ModelTransform.pivot(-1.5F, -4.0F, 0.5F));
 
-                ModelPartData rotationAssembly = rotation.addChild("rotation_assembly",
+                ModelPartData rotation = rotationRoot.addChild("rotation",
                                 ModelPartBuilder.create().uv(8, 31).cuboid(-9.0F, -6.0F, -1.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F))
                                                 .uv(12, 31).cuboid(11.0F, -3.0F, -1.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F))
                                                 .uv(0, 27).cuboid(1.0F, -6.0F, -1.0F, 1.0F, 5.0F, 1.0F, new Dilation(0.0F)),
                                 ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-                rotationAssembly.addChild("cube_r1", ModelPartBuilder.create().uv(24, 11).cuboid(0.0F, -8.0F, 0.0F, 1.0F, 10.0F, 1.0F,
+                rotation.addChild("cube_r1", ModelPartBuilder.create().uv(24, 11).cuboid(0.0F, -8.0F, 0.0F, 1.0F, 10.0F, 1.0F,
                                 new Dilation(0.0F)), ModelTransform.of(10.0F, -3.0F, -1.0F, 0.0F, 0.0F, -1.5708F));
 
-                rotationAssembly.addChild("cube_r2", ModelPartBuilder.create().uv(24, 0).cuboid(0.0F, -10.0F, 0.0F, 1.0F, 10.0F, 1.0F,
+                rotation.addChild("cube_r2", ModelPartBuilder.create().uv(24, 0).cuboid(0.0F, -10.0F, 0.0F, 1.0F, 10.0F, 1.0F,
                                 new Dilation(0.0F)), ModelTransform.of(1.0F, -3.0F, -1.0F, 0.0F, 0.0F, -1.5708F));
 
-                rotationAssembly.addChild("cap3",
+                rotation.addChild("cap3",
                                 ModelPartBuilder.create().uv(22, 28).cuboid(-1.0F, -2.0F, -1.0F, 2.0F, 1.0F, 1.0F,
                                                 new Dilation(0.0F))
                                                 .uv(28, 10).cuboid(0.5F, -2.0F, 0.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F))
@@ -67,7 +71,7 @@ public class SprinklerModel extends EntityModel<Entity> {
                                                 .uv(28, 19).cuboid(0.5F, -2.0F, -3.0F, 1.0F, 1.0F, 2.0F, new Dilation(0.0F)),
                                 ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-                rotationAssembly.addChild("cap2",
+                rotation.addChild("cap2",
                                 ModelPartBuilder.create().uv(16, 31).cuboid(0.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F,
                                                 new Dilation(0.0F))
                                                 .uv(32, 22).cuboid(0.5F, -1.0F, 0.0F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F))
@@ -119,14 +123,16 @@ public class SprinklerModel extends EntityModel<Entity> {
         }
 
         public void setAnimationProgress(float animationProgress) {
-                this.rotationAngle = animationProgress * ((float) Math.PI * 2.0F);
+                this.rotationAngle = this.rotationAnimation.sampleYaw(animationProgress);
         }
 
         @Override
         public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green,
                         float blue, float alpha) {
+                this.rotationRoot.resetTransform();
+                this.rotation.resetTransform();
                 this.rotation.yaw = this.rotationAngle;
-                this.rotation.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+                this.rotationRoot.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
                 this.cap4.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
                 this.bbMain.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
         }
