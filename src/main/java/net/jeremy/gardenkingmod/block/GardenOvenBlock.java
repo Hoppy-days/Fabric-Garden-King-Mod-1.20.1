58bcbac7d35fc9c9fb09db3c31fd50bfdd46165e
Hoppy-days <fifthstreetjammer@gmail.com>
2025-10-25 19:05:46 -0500

    Align garden oven placement facing with player

--- src/main/java/net/jeremy/gardenkingmod/block/GardenOvenBlock.java
+++ src/main/java/net/jeremy/gardenkingmod/block/GardenOvenBlock.java
@@ -2,10 +2,13 @@ package net.jeremy.gardenkingmod.block;
 
 import net.jeremy.gardenkingmod.block.entity.GardenOvenBlockEntity;
 import net.minecraft.block.AbstractFurnaceBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -19,17 +22,35 @@ public class GardenOvenBlock extends AbstractFurnaceBlock {
                                 .with(LIT, Boolean.FALSE));
         }
 
-        @Override
-        public BlockState getPlacementState(ItemPlacementContext ctx) {
-                Direction facing = ctx.getHorizontalPlayerFacing().getOpposite();
-                return this.getDefaultState().with(FACING, facing).with(LIT, Boolean.FALSE);
-        }
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		Direction facing = ctx.getHorizontalPlayerFacing().getOpposite();
+		return this.getDefaultState().with(FACING, facing).with(LIT, Boolean.FALSE);
+	}
 
-        @Override
-        protected void openScreen(World world, BlockPos pos, PlayerEntity player) {
-                if (world.isClient) {
-                        return;
-                }
+	@Override
+	public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
+		super.onPlaced(world, pos, state, placer, itemStack);
+		if (world.isClient) {
+			return;
+		}
+
+		Direction facing = state.get(FACING);
+		if (placer instanceof PlayerEntity player) {
+			facing = player.getHorizontalFacing().getOpposite();
+		}
+
+		BlockState updatedState = state.with(FACING, facing);
+		if (!updatedState.equals(state)) {
+			world.setBlockState(pos, updatedState, Block.NOTIFY_ALL);
+		}
+	}
+
+	@Override
+	protected void openScreen(World world, BlockPos pos, PlayerEntity player) {
+		if (world.isClient) {
+			return;
+		}
 
                 BlockEntity blockEntity = world.getBlockEntity(pos);
                 if (blockEntity instanceof GardenOvenBlockEntity oven) {
