04590b2aa892d722cc9e4098bebc1d64a0ca1f15
Hoppy-days <fifthstreetjammer@gmail.com>
2026-02-17 00:18:42 -0600

    Allow Sharpness books to apply above vanilla max

---
+++ src/main/java/net/jeremy/gardenkingmod/mixin/EnchantmentMixin.java
@@ -0,0 +1,24 @@
+package net.jeremy.gardenkingmod.mixin;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Enchantment.class)
+public abstract class EnchantmentMixin {
+    private static final Identifier SHARPNESS_ID = new Identifier("minecraft", "sharpness");
+    private static final int SHARPNESS_MAX_LEVEL = 10;
+
+    @Inject(method = "getMaxLevel", at = @At("RETURN"), cancellable = true)
+    private void gardenkingmod$allowHigherCustomLevels(CallbackInfoReturnable<Integer> cir) {
+        Enchantment enchantment = (Enchantment) (Object) this;
+        Identifier enchantmentId = Registries.ENCHANTMENT.getId(enchantment);
+        if (SHARPNESS_ID.equals(enchantmentId) && cir.getReturnValue() < SHARPNESS_MAX_LEVEL) {
+            cir.setReturnValue(SHARPNESS_MAX_LEVEL);
+        }
+    }
+}
