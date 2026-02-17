e66dca3d66ac52b1b4f1ee9cbf897e0d587408fd
Hoppy-days <fifthstreetjammer@gmail.com>
2026-02-17 07:51:48 -0600

    Fix market buy-tab actions when block entity is unavailable

--- src/main/java/net/jeremy/gardenkingmod/screen/MarketScreenHandler.java
+++ src/main/java/net/jeremy/gardenkingmod/screen/MarketScreenHandler.java
@@ -460,41 +460,39 @@ public class MarketScreenHandler extends ScreenHandler {
                         return true;
                 }
 
-                if (this.blockEntity != null) {
-                        if (id == BUTTON_SELL && player instanceof ServerPlayerEntity serverPlayer) {
-                                if (this.blockEntity.sell(serverPlayer)) {
+                if (id == BUTTON_SELL && player instanceof ServerPlayerEntity serverPlayer) {
+                        if (this.blockEntity != null && this.blockEntity.sell(serverPlayer)) {
+                                sendContentUpdates();
+                        }
+                        return true;
+                }
+
+                if (id == BUTTON_SELECT_SELL_TAB) {
+                        if (player instanceof ServerPlayerEntity serverPlayer) {
+                                if (clearSelection(serverPlayer)) {
                                         sendContentUpdates();
                                 }
-                                return true;
                         }
+                        setMarketSlotsEnabled(true);
+                        setBuySlotsEnabled(false);
+                        return true;
+                }
 
-                        if (id == BUTTON_SELECT_SELL_TAB) {
-                                if (player instanceof ServerPlayerEntity serverPlayer) {
-                                        if (clearSelection(serverPlayer)) {
-                                                sendContentUpdates();
-                                        }
-                                }
-                                setMarketSlotsEnabled(true);
-                                setBuySlotsEnabled(false);
-                                return true;
+                if (id == BUTTON_SELECT_BUY_TAB) {
+                        boolean returnedItems = this.blockEntity != null && this.blockEntity.returnItemsToPlayer(player);
+                        setMarketSlotsEnabled(false);
+                        setBuySlotsEnabled(true);
+                        if (returnedItems) {
+                                sendContentUpdates();
                         }
+                        return true;
+                }
 
-                        if (id == BUTTON_SELECT_BUY_TAB) {
-                                boolean returnedItems = this.blockEntity.returnItemsToPlayer(player);
-                                setMarketSlotsEnabled(false);
-                                setBuySlotsEnabled(true);
-                                if (returnedItems) {
-                                        sendContentUpdates();
-                                }
-                                return true;
+                if (id == BUTTON_BUY && player instanceof ServerPlayerEntity serverPlayer) {
+                        if (processPurchase(serverPlayer, this.selectedOfferIndex, false)) {
+                                sendContentUpdates();
                         }
-
-                        if (id == BUTTON_BUY && player instanceof ServerPlayerEntity serverPlayer) {
-                                if (processPurchase(serverPlayer, this.selectedOfferIndex, false)) {
-                                        sendContentUpdates();
-                                }
-                                return true;
-                        }
+                        return true;
                 }
 
                 return super.onButtonClick(player, id);
